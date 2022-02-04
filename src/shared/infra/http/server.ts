5ee2d2418cf2ef8b205e887c2271b6934216a149
Vinicius Pinto <viniciuspinto10vsp@gmail.com>
2022-02-03 21:15:16 -0300

    fixed problem with appError -> missing nextFunction

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import '@shared/container';
 import 'express-async-errors';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { routes } from './routes';
 import { createConnection } from 'typeorm';
 import { AppError } from '@shared/errors/AppError';
@@ -14,7 +14,8 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((err: Error, req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof AppError) {
 		return res.status(err.statusCode).json({
 			message: err.message,
